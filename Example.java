f53b215bc30f54dddbb697c82a4b11cfd4cc9210
neha-chavan17 <nr.chavan17@gmail.com>
2021-04-12 17:04:46 +0530

    This is push

---
+++ Example.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Example {
+   public static void main(String argu[]) {
+Scanner sc=new Scanner(System.in);
+      String str = sc.next();
+      char[] carray = str.toCharArray();
+      System.out.println("The string is:" + str);
+      System.out.print("Duplicate Characters in above string are: ");
+      for (int i = 0; i < str.length(); i++) {
+         for (int j = i + 1; j < str.length(); j++) {
+            if (carray[i] == carray[j]) {
+               System.out.print(carray[j] + " ");
+               break;
+            }
+         }
+      }
+   }
+}
